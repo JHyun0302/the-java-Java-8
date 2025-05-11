5b47cd507012825496ea154262f885a2b00a4a7e
JHyun0302 <wogustkfkd11@gmail.com>
2025-05-11 15:57:47 +0900

    :sparkles: [Feat] 배열 병렬 정렬

---
+++ src/main/java/me/whiteship/java8to11/chapter8/chap2/App.java
@@ -0,0 +1,23 @@
+package me.whiteship.java8to11.chapter8.chap2;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class App {
+    public static void main(String[] args) {
+        int size = 1500;
+        int[] numbers = new int[size];
+        Random random = new Random();
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+
+        long start = System.nanoTime();
+        Arrays.sort(numbers);
+        System.out.println("serial sorting took " + (System.nanoTime() - start));
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers); // 배열을 잘라서 정렬하고 다시 붙이는 방식
+        System.out.println("parallel sorting took " + (System.nanoTime() - start));
+    }
+}
